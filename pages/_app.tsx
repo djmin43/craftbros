261ff8ed32d531e6184c752a12b6bb7a799265f5
jay <dj.min43@gmail.com>
2022-04-30 13:36:47 +0900

    add kakao map

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Script from "next/script";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=6887e18eea343d9ecb3cc5e964acd13b&libraries=services,clusterer&autoload=false"
+        strategy="beforeInteractive"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
