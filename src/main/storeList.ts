641acfc1aa9fa3d3971a97d1d7532dfab84a7d51
jay <dj.min43@gmail.com>
2022-04-30 20:59:21 +0900

    add more stores with coords

--- src/main/storeList.ts
+++ src/main/storeList.ts
@@ -16,9 +16,30 @@ export const storeList: StoreInfo[] = [
   },
   {
     title: "크래프트브로스 서래",
-    lat: 37.4559,
-    lng: 126.9723,
+    lat: 37.4987451,
+    lng: 126.9976208,
     description: "추가내용을 입력해주세요",
     storeId: 24712107,
+  },
+  {
+    title: "브루하임",
+    lat: 37.5502921,
+    lng: 126.918143,
+    description: "추가내용을 입력해주세요",
+    storeId: 421582356,
+  },
+  {
+    title: "케그샵",
+    lat: 37.5179612,
+    lng: 126.9328641,
+    description: "추가내용을 입력해주세요",
+    storeId: 167948055,
+  },
+  {
+    title: "브루어스",
+    lat: 37.5472632,
+    lng: 126.9840035,
+    description: "추가내용을 입력해주세요",
+    storeId: 444745978,
   },
 ];
