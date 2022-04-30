583ae0e740f521f44cca0ecb806417bd37889f72
jay <dj.min43@gmail.com>
2022-04-30 13:43:05 +0900

    remove comment

--- src/main/KakaoMap.tsx
+++ src/main/KakaoMap.tsx
@@ -9,10 +9,10 @@ const KakaoMap = () => {
     const { kakao }: any = window;
     kakao.maps.load(function () {
       const options = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667), //지도의 중심좌표.
-        level: 3, //지도의 레벨(확대, 축소 정도)
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
       };
-      new kakao.maps.Map(mapContainer.current, options); //지도 생성 및 객체 리턴
+      new kakao.maps.Map(mapContainer.current, options);
     });
   }, []);
 
