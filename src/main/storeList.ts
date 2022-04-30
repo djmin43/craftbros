beac726630cb62c3fc4c44fa0d0c4d5f3761a3fe
jay <dj.min43@gmail.com>
2022-04-30 20:22:02 +0900

    refactor markers

---
+++ src/main/storeList.ts
@@ -0,0 +1,24 @@
+export interface StoreInfo {
+  title: string;
+  lat: number;
+  lng: number;
+  description: string;
+  storeId: number;
+}
+
+export const storeList: StoreInfo[] = [
+  {
+    title: "브릭하우스76",
+    lat: 37.6027028,
+    lng: 126.9160588,
+    description: "맥주리스트: 원더페일에일, 슈퍼IPA",
+    storeId: 27300595,
+  },
+  {
+    title: "크래프트브로스 서래",
+    lat: 37.4559,
+    lng: 126.9723,
+    description: "추가내용을 입력해주세요",
+    storeId: 24712107,
+  },
+];
