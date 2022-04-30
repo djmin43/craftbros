2a899b3f96b2c923fcb1f84b1ec60eaed44465d3
jay <dj.min43@gmail.com>
2022-04-30 17:11:08 +0900

    add markers with info

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: ["instagram.ficn3-4.fna.fbcdn.net"],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
